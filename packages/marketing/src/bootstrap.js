242c2ec94b4f61924a9620d7aa736f92c8c19d33
colbychounm <nmc.1909@gmail.com>
2025-01-15 22:54:04 +0700

    routing and communicating between container and marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  createBrowserHistory,
+  createMemoryHistory,
+} from "../node_modules/.pnpm/history@4.10.1/node_modules/history";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marking-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
